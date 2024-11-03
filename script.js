8e76cc4be00214ed44fcbf2c92356d20eb64bdcb
Lucas Santanna <58341966+1-galahad@users.noreply.github.com>
2024-11-03 08:41:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Bem-vindo ao site da Stratos!");
+// Ativar/desativar Modo Escuro
+const darkModeToggle = document.getElementById('darkModeToggle');
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    const socialButtons = document.querySelectorAll('.social-buttons a');
+    socialButtons.forEach(button => button.classList.toggle('dark-mode'));
+
+    // Alterna o ícone do botão de modo escuro
+    darkModeToggle.textContent = document.body.classList.contains('dark-mode') ? '🌞 Modo Claro' : '🌙 Modo Escuro';
+});
