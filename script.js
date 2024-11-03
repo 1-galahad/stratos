c8bc1481e4bd6c571b5c919704aab105c03bf009
Lucas Santanna <58341966+1-galahad@users.noreply.github.com>
2024-11-03 08:51:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-// Ativar/desativar Modo Escuro
+// Ativar/desativar Modo Escuro com efeito deslizante
 const darkModeToggle = document.getElementById('darkModeToggle');
 darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
-
-    const socialButtons = document.querySelectorAll('.social-buttons a');
-    socialButtons.forEach(button => button.classList.toggle('dark-mode'));
-
-    // Alterna o ícone do botão de modo escuro
-    darkModeToggle.textContent = document.body.classList.contains('dark-mode') ? '🌞 Modo Claro' : '🌙 Modo Escuro';
+    darkModeToggle.classList.toggle('active');
 });
