7d04aa04b029ab6a6b7a1dbd846ed847ad0ad10c
Lucas Santanna <lucas.santanna.f@gmail.com>
2024-11-07 09:45:31 -0300

    changes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Função para alternar o menu dropdown
 function toggleMenu() {
     const menu = document.getElementById('dropdown-menu');
     menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
