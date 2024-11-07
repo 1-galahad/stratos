3e4bc7633ac66f6a2e3a8fb406add9cdd7ac7f8a
Lucas Santanna <lucas.santanna.f@gmail.com>
2024-11-07 07:52:14 -0300

    testando layout a gelaeria

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
-let currentSlide = 0;
-
-function showSlide(index) {
-    const slides = document.querySelector('.slides');
-    const totalSlides = document.querySelectorAll('.slide').length;
-
-    if (index >= totalSlides) {
-        currentSlide = 0; 
-    } else if (index < 0) {
-        currentSlide = totalSlides - 1; 
-    } else {
-        currentSlide = index;
-    }
-
-    slides.style.transform = 'translateX(' + (-currentSlide * 100) + '%)';
-}
-
-setInterval(() => {
-    showSlide(currentSlide + 1);
-}, 5000);
-
-function toggleDarkMode() {
-    document.body.classList.toggle('dark-mode');
-    const toggleCircle = document.querySelector('.toggle-circle');
-    
-    if (document.body.classList.contains('dark-mode')) {
-        toggleCircle.style.transform = 'translateX(20px)';
-    } else {
-        toggleCircle.style.transform = 'translateX(0)';
-    }
+function toggleMenu() {
+    const menu = document.getElementById('dropdown-menu');
+    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
 }
